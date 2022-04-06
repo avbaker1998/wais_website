9c41d558b83d67adf11399f3e2bf559c3d2963a7
avbaker1998 <70988383+avbaker1998@users.noreply.github.com>
2022-04-06 17:24:18 -0600

    first edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,12 @@ import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <Seo title="WAIS Home" />
+    <h1>BYU WAIS</h1>
+    <h3>Women in Information Systems</h3>
+    <p>For women, built by women</p>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
+      src="../images/women-in-tech.jpg"
       width={300}
       quality={95}
       formats={["auto", "webp", "avif"]}
@@ -20,7 +20,7 @@ const IndexPage = () => (
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/page-2/">Go to page 2</Link> |
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
       <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
       <Link to="/using-dsg">Go to "Using DSG"</Link>
